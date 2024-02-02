074ef341e519163cc8e8d47049ac58644c1d4974
OleksandraTsy <aleksashka0581@gmail.com>
2024-02-02 20:14:50 +0100

    js 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,24 @@
-function filterArray(numbers, value) {
-
-    // Створи порожній масив, у який будеш додавати підходящі числа.
-    const resultArray = [];
-
-    // Використай цикл для ітерації кожного елемента масиву numbers.
-    for (let i = 0; i < numbers.length; i += 1){
-        const number = numbers[i];
-
-        // Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
-        if (number > value) {
-        resultArray.push(number);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+  
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
     }
 
-// Поверни свій новий масив з підходящими числами як результат.
-return resultArray;
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
